f7b490533d3a0c516251a026f9884e951185c73d
arvinxx <arvinx@foxmail.com>
2025-07-28 22:13:16 +0800

    make pglite db default

--- src/const/version.ts
+++ src/const/version.ts
@@ -5,7 +5,7 @@ import { BRANDING_NAME, ORG_NAME } from './branding';
 export const CURRENT_VERSION = pkg.version;
 
 export const isServerMode = process.env.NEXT_PUBLIC_SERVICE_MODE === 'server';
-export const isUsePgliteDB = process.env.NEXT_PUBLIC_CLIENT_DB === 'pglite';
+export const isUsePgliteDB = true;
 
 export const isDesktop = process.env.NEXT_PUBLIC_IS_DESKTOP_APP === '1';
 
